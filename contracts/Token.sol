de4421eaccfb8d8b34ae490475b72606273948cf
Michael Bumann <hello@michaelbumann.com>
2021-05-11 01:06:15 +0200

    Make token enumerable

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,11 +2,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Token is ERC721, Ownable {
+contract Token is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
